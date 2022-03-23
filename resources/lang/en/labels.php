1a5f3e2fa8e428614fcc2d2ad90d0e54aeaecc1a
hieutd231000 <hieutd2310@gmail.com>
2022-03-23 15:38:54 +0700

    bar update

--- resources/lang/en/labels.php
+++ resources/lang/en/labels.php
@@ -2,7 +2,11 @@
 return [
     'footer_text' => 'All rights reserved.',
     'side_bar' => [
+        'title' => 'ZIME FILE',
+        'name'  => 'Admin',
         'item_1' => 'Dashboard',
-        'item_2' => '',
+        'item_2' => 'Packages',
+        'item_3' => 'Files',
+        'item_4' => 'Users',
     ],
 ];
--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -3,13 +3,18 @@
     <!-- // TODO logout icon here -->
     <ul class="navbar-nav">
         <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button">
+                <i class="fas fa-bars"></i>
+            </a>
         </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="index3.html" class="nav-link">{{ trans("labels.navbar_content1") }}</a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link">{{ trans("labels.navbar_content2") }}</a>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="" href="#" role="button">
+                <i class="fas fa-sign-out-alt"></i>
+            </a>
         </li>
     </ul>
 </nav>
