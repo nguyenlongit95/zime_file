f1c6643590a05c4a55efd665de8448ee4e490898
hieutd231000 <hieutd2310@gmail.com>
2022-03-23 16:06:21 +0700

    delete comment

--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -1,6 +1,5 @@
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
-    <!-- // TODO logout icon here -->
     <ul class="navbar-nav">
         <li class="nav-item">
             <a class="nav-link" data-widget="pushmenu" href="#" role="button">
