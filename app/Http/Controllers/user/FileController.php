803c071e08869ba8cbc65d19f0b651a861d9f9ec
hieutd231000 <hieutd2310@gmail.com>
2022-04-20 16:19:45 +0700

    fix style

--- app/Http/Controllers/user/FileController.php
+++ app/Http/Controllers/user/FileController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 
 class FileController extends Controller
 {
+    /**
+     * View user's file manage
+     *
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function fileManage(Request $request)
     {
         return view('user.file');
--- app/Http/Controllers/user/PackageController.php
+++ app/Http/Controllers/user/PackageController.php
@@ -81,6 +81,5 @@ class PackageController extends Controller
         } catch (\Exception $exception) {
             Log::error($exception->getMessage());
         }
-
     }
 }
